9d276d61a78b702dfb979b1bd2715b741c2ab226
Николай <Kolya.Balancev@yandex.ru>
2021-03-09 01:18:25 +0300

    update

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 
-void delFirstMethod(int x[], int n, int key) {
+void delFirstMethod(int *x[], int n, int key) {
     int i = 1;
     while (i <= n) {
-        if (x[i] == key) {
-            // del
+        if (*x[i] == key) {
+            cout << "del";
             for (int j = 0; j < n - 1; ++j)
                 x[j] = x[j + 1];
             break;
@@ -17,10 +17,18 @@ void delFirstMethod(int x[], int n, int key) {
 }
 
 int main() {
-    int n;
+    int n, key;
     cout << "Введите количество элементов исходного  массива: ";
     cin >> n;
+    int *x = new int[n];
     srand(time(nullptr));
-    cout << rand() % 10;
+    cout << "Введите удалемый элемент: ";
+    cin >> key;
+    for (int i = 0; i < n; ++i)
+        x[i] = rand() % 10;
+    cout << "Исходный массив:" << endl;
+    for (int i = 0; i < n; ++i)
+        cout << x[i] << endl;
+    delFirstMethod(x, n, key);
     return 0;
 }
